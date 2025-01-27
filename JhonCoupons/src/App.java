1a6ed240021d701f08bd5ae649169ebebeb0aaa0
noam shalev <noamsbest@gmail.com>
2025-01-27 18:16:19 +0200

    Fix several bugs

--- JhonCoupons/src/App.java
+++ JhonCoupons/src/App.java
@@ -14,6 +14,7 @@ public class App {
     public static void main(String[] args) throws SQLException, InvalidUserInfoException, TitleAlreadyExistException, ParseException, UnablePurchaseException, CompanyAlreadyExistException, EmailAlreadyExistException {
 
         Testing.testAll();
+        System.out.println("server is running");
 
     }
 }
